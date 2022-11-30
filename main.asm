961bab899d1be8bc86dbbac8da19d70cc85a8c2a
aliaa gheis <aliaagheis@gmail.com>
2022-12-01 01:34:02 +0200

    code

---
+++ main.asm
@@ -0,0 +1,114 @@
+;Author: Aliaa Gheis
+;DATE:
+;This Progam
+;======================
+        PUBLIC color, boardWidth, imageWidth
+;======================
+        .286
+        include graphics.inc
+        .MODEL SMALL
+        .STACK 64
+        .DATA
+color           db 15, 9
+boardWidth      equ 25
+imageWidth      equ 25  
+; ____ game peice ____ ;
+emptyCell       equ 0
+pawn            equ 1
+rook            equ 2
+knight          equ 3
+bishop          equ 4
+queen           equ 5
+king            equ 6
+; ____ peice color ____ ;
+black           equ 8
+white           equ 0
+; ____ peice mask ____ ;
+peice           equ 7
+; ____ board ____ ;
+board           db  rook+black, knight+black, bishop+black, queen+black, king+black, bishop+black, knight+black, rook+black
+                db  8 dup(pawn+black)
+                db  4 dup( 8 dup ( emptyCell) )
+                db  8 dup(pawn)
+                db  rook, knight, bishop, queen, king, bishop, knight, rook
+
+
+bishopimg DB 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 16, 16, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 16, 16, 16, 16, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 16, 15, 15, 16, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 16, 16, 16, 16, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 4, 4, 16, 16, 16, 16, 16, 16, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 4, 16, 16, 15, 15, 15, 15, 16, 16, 4, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 16, 16, 15, 15, 15, 15, 15, 15, 16, 16, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 16, 15, 15, 15, 16, 16, 15, 15, 15, 16, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 16, 16, 15, 15, 15, 16, 16, 15, 15, 15, 16, 16, 4, 4, 4, 4, 4, 4, 4        
+          DB 4, 4, 4, 4, 4, 4, 16, 15, 15, 15, 15, 15, 15, 15, 15, 15, 16, 16, 4, 4, 4, 4, 4, 4, 4        
+          DB 4, 4, 4, 4, 4, 4, 16, 16, 15, 15, 15, 15, 15, 15, 15, 15, 16, 16, 4, 4, 4, 4, 4, 4, 4        
+          DB 4, 4, 4, 4, 4, 4, 16, 16, 15, 15, 15, 15, 15, 15, 15, 15, 16, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 16, 16, 16, 16, 16, 16, 16, 16, 16, 4, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 16, 16, 15, 15, 15, 15, 15, 15, 16, 16, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 16, 16, 15, 15, 15, 15, 15, 15, 16, 16, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 16, 16, 16, 16, 15, 15, 16, 16, 16, 16, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 4, 4, 4, 4, 4   
+          DB 4, 4, 16, 16, 16, 15, 16, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 16, 15, 16, 16, 16, 4, 4, 4
+          DB 4, 4, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 4, 4, 4
+          DB 4, 4, 16, 16, 16, 16, 16, 16, 16, 16, 16, 4, 4, 16, 16, 16, 16, 16, 16, 16, 16, 16, 4, 4, 4  
+          DB 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+          DB 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+ 
+
+        .CODE
+
+MAIN    PROC FAR
+        MOV AX, @DATA
+        MOV DS, AX
+
+        mov ax, 0a000h  ;for inline drawing        
+        mov es, ax
+
+        ; ____ inialize video mode ____;
+        CLRS            ; clear screen
+        mov ax, 0013h   ; to video mode        
+        int 10h
+        
+        
+        DrawGrid boardWidth, color
+
+
+        DrawImg bishopimg, imageWidth, 25
+            
+
+
+        ;__end___;
+        CALL waitSec
+        CALL waitSec
+        MOV AH, 4CH
+        INT 21H
+MAIN    ENDP
+
+clcWhere    PROC  ;ax = row, cx = col =>>>> ax = current start point
+push dx
+
+            mov dx, 320
+            mul dx
+            add ax, cx
+            ;ax = current start point
+pop dx
+RET
+ENDP    clcWhere
+
+
+waitSec     PROC  ;ax = row, cx = col =>>>> ax = current start point
+            push ax
+
+            mov ah, 07
+            int 21h
+
+            pop ax
+            RET
+ENDP    waitSec
+
+END MAIN
